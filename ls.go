4f92cef99edd434d81cb92b09c77962ef8b2c6fe
ravelll <ravelll.taniguchi@gmail.com>
2016-08-14 23:33:25 +0900

    make code cleaner

--- ls.go
+++ ls.go
@@ -7,8 +7,18 @@ import (
 )
 
 func main() {
+	var dirs []string
+
 	if len(os.Args) == 1 {
+		// the case executed with no args
 		dir, _ := os.Getwd()
+		dirs = append(dirs, dir)
+	} else {
+		// the case executed with args (= dirs to list)
+		dirs = os.Args[1:]
+	}
+
+	for _, dir := range dirs {
 		files, err := ioutil.ReadDir(dir)
 
 		if err != nil {
@@ -17,18 +27,6 @@ func main() {
 
 		for _, file := range files {
 			fmt.Printf("%s\t", file.Name())
-		}
-	} else {
-		for _, arg := range os.Args[1:] {
-			files, err := ioutil.ReadDir(arg)
-
-			if err != nil {
-				fmt.Printf("%s\n", err.Error())
-			}
-
-			for _, file := range files {
-				fmt.Printf("%s\t", file.Name())
-			}
 		}
 	}
 }
